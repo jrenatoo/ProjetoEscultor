8be3b7402a2232f63465562f004d9c1c35ef49ff
Renato Ribeiro <joserenato2014@hotmail.com>
2024-11-29 07:47:50 -0300

    Create trono.cpp

---
+++ trono.cpp
@@ -0,0 +1,13 @@
+#include "sculptor.hpp"
+#include <iostream>
+#include <fstream>
+int main(void){
+    
+    Sculptor trono(10,10,10);
+
+    trono.setColor(0,0,1.0,1.0);
+    trono.putBox(0,9,0,9,0,9);
+    trono.cutBox(1,8,1,9,1,9);
+    trono.writeOFF("trono.off");
+    return 0;
+}
